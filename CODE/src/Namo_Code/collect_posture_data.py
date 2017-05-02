65dc9957f7b3e1ef9244c11e075b4dd4026cda86
saintwings <wisanu.jutharee@gmail.com>
2017-05-02 17:15:11 +0700

    update s invite postures

--- CODE/src/Namo_Code/collect_posture_data.py
+++ CODE/src/Namo_Code/collect_posture_data.py
@@ -7,3 +7,18 @@ path = './Postures/posture_set_bye'
 for filename in glob.glob(os.path.join(path, '*.ini')):
     print(filename)
 
+
+    config = ConfigObj(filename)
+    print(config['Keyframe_Time'])
+    # self.int_numberOfKeyframe = int(config['Keyframe_Amount'])
+    # self.ui.numOfKeyframeStatus_label.setText(str(self.int_numberOfKeyframe))
+    #
+    # for i in range(int(self.int_numberOfKeyframe)):
+    #     self.bool_activeKeyframe[i] = True
+    #     self.int_motorValue[i] = list(map(int, config['Keyframe_Value']['Keyframe_' + str(i)]))
+    #
+    #     self.int_time[i] = int(config['Keyframe_Time'][i])
+    #
+    # for i in range(int(self.int_numberOfKeyframe), 30):
+    #     self.bool_activeKeyframe[i] = False
+
